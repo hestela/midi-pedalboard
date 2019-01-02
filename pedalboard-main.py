ce3a1ba43bf2ed5f751fd2e5bd94fd0bc9109a37
Brogan O'Hara <oharabrogan@gmail.com>
2019-01-02 07:24:49 +0000

    Add working GPIO file

---
+++ pedalboard-main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+from functools import partial
+
+class state_obj():
+    def __init__(self):
+        self.curr_state = 0
+        self.last_msg_sent = 1
+
+def main():
+    try:
+        GPIO.setmode(GPIO.BCM)
+
+        # GPIO 24 set up as an input, pulled down, connected to 3V3 on button press
+        GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+        # now we'll define two threaded callback functions
+        # these will run in another thread when our events are detected
+        def button_callback(state, channel):
+            if state.last_msg_sent == 1:
+                state.curr_state = 2
+            elif state.last_msg_sent == 2:
+                state.curr_state = 1
+
+        state = state_obj()
+        f = partial(button_callback, state)
+
+        # when a rising edge is detected on gpio 4, regardless of whatever
+        # else is happening in the program, the function my_callback will be run
+        GPIO.add_event_detect(4, GPIO.RISING, callback=f, bouncetime=300)
+
+        while True:
+            if state.curr_state == 0:
+                time.sleep(0.0002)
+            elif state.curr_state == 1:
+                # send midi message 1
+                state.last_msg_sent = 1
+                state.curr_state = 0
+            elif state.curr_state == 2:
+                # send midi message 2
+                state.last_msg_sent = 2
+                state.curr_state = 0
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+    GPIO.cleanup()
+if __name__ == '__main__':
+    main()
