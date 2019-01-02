16d9a08736d566a08766a20f8c1c91cb3f37b442
Brogan O'Hara <oharabrogan@gmail.com>
2019-01-02 07:47:21 +0000

    Add midi message sending functionality

--- pedalboard-main.py
+++ pedalboard-main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 import time
+import rtmidi
+import mido
 from functools import partial
 
 class state_obj():
@@ -10,6 +12,11 @@ class state_obj():
 
 def main():
     try:
+        midiout = rtmidi.MidiOut()
+
+        # FIXME hardcoded '2' for microKORG XL
+        midiout.open_port(2)
+
         GPIO.setmode(GPIO.BCM)
 
         # GPIO 24 set up as an input, pulled down, connected to 3V3 on button press
@@ -30,20 +37,27 @@ def main():
         # else is happening in the program, the function my_callback will be run
         GPIO.add_event_detect(4, GPIO.RISING, callback=f, bouncetime=300)
 
+        msg_one = [192, 5]
+        msg_two = [192, 57]
+
         while True:
             if state.curr_state == 0:
                 time.sleep(0.0002)
             elif state.curr_state == 1:
                 # send midi message 1
+                midiout.send_message(msg_one)
                 state.last_msg_sent = 1
                 state.curr_state = 0
             elif state.curr_state == 2:
                 # send midi message 2
+                midiout.send_message(msg_two)
                 state.last_msg_sent = 2
                 state.curr_state = 0
 
     except KeyboardInterrupt:
         GPIO.cleanup()       # clean up GPIO on CTRL+C exit
     GPIO.cleanup()
+
+
 if __name__ == '__main__':
     main()
